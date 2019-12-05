e3657b5fb29bf3ccb0985e357821ad75ed6185c0
Mia <tamiapaschall@gmail.com>
2019-12-05 04:08:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,3 @@
 def oxford_comma(array)
-
-  if array.length < 2
-end 	    array.join
-  elsif array.length == 2
-    array.insert(1, "and").join(" ")
-  else
-    str = ", and " + array.pop
-    str = array.join(", ") << str
-  end
+[array].join(",")
 end
